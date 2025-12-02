ec2f11a13e610f6c24cafed67c5e439f5296674c
sajjad-FL <sajjad.alam@fissionlabs.com>
2025-12-02 17:00:13 +0530

    code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // No "output: export"
-  output: 'export',
+  output: 'export', // enable static export
 };
 
 module.exports = nextConfig;
